6b4ff370c24c1c1ce5f44db23e48f2cae992990e
Dennis Luitwieler <dennisluitwieler@gmail.com>
2025-04-19 14:02:39 +0200

    Make unit test run

--- libs/mg-frontend/list/src/lib/list.component.spec.ts
+++ libs/mg-frontend/list/src/lib/list.component.spec.ts
@@ -1,4 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { GIFT_REPOSITORY } from './gift-repository-token';
+import { InMemoryGiftRepository } from './in-memory-gift.repository';
 import { ListComponent } from './list.component';
 
 describe('ListComponent', () => {
@@ -8,6 +10,12 @@ describe('ListComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [ListComponent],
+      providers: [
+        {
+          provide: GIFT_REPOSITORY,
+          useClass: InMemoryGiftRepository,
+        }
+      ]
     }).compileComponents();
 
     fixture = TestBed.createComponent(ListComponent);
